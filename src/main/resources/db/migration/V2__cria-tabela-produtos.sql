bd8936b0958a95d48446b3ad27733ef41a762394
alexandrejnrx <alexandre.jnrx@gmail.com>
2025-10-15 16:45:01 -0300

    adiciona migration para tabela produtos

---
+++ src/main/resources/db/migration/V2__cria-tabela-produtos.sql
@@ -0,0 +1,10 @@
+CREATE TABLE produtos
+(
+    id                    INT AUTO_INCREMENT PRIMARY KEY,
+    numero_unico_producao INT          NOT NULL UNIQUE,
+    serial                INT UNIQUE,
+    data_producao         DATE,
+    lote                  INT          NOT NULL,
+    responsavel           VARCHAR(100) NOT NULL
+
+);
