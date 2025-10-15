d66a2a1f86cee1bfe6beeefa4b0c35fe620e0fa1
alexandrejnrx <alexandre.jnrx@gmail.com>
2025-10-15 16:39:40 -0300

    ajusta estrutura do projeto

--- src/main/java/br/com/alexandrejnrx/controlprodapi/configuration/ConfiguracaoSeguranca.java
+++ src/main/java/br/com/alexandrejnrx/controlprodapi/configuration/ConfiguracaoSeguranca.java
@@ -1,4 +1,4 @@
-package br.com.alexandrejnrx.controlprodapi.controller;
+package br.com.alexandrejnrx.controlprodapi.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
