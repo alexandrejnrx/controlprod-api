b475373e067a922557385cac418ecb73e49d28ad
alexandrejnrx <alexandre.jnrx@gmail.com>
2025-10-15 11:21:26 -0300

    formata classes

--- src/main/java/br/com/alexandrejnrx/controlprodapi/dto/UsuarioRequestDTO.java
+++ src/main/java/br/com/alexandrejnrx/controlprodapi/dto/UsuarioRequestDTO.java
@@ -1,20 +1,20 @@
-    package br.com.alexandrejnrx.controlprodapi.dto;
+package br.com.alexandrejnrx.controlprodapi.dto;
 
-    import jakarta.validation.constraints.NotBlank;
-    import lombok.Getter;
-    import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
-    @Getter
-    @Setter
-    public class UsuarioRequestDTO {
+@Getter
+@Setter
+public class UsuarioRequestDTO {
 
-        @NotBlank(message = "O campo nome não pode ser vazio.")
-        private String nome;
-        @NotBlank(message = "O campo nome de usuário não pode ser vazio.")
-        private String nomeUsuario;
-        @NotBlank(message = "O campo e-mail não pode ser vazio.")
-        private String email;
-        @NotBlank(message = "O campo senha não pode ser vazio.")
-        private String senha;
+    @NotBlank(message = "O campo nome não pode ser vazio.")
+    private String nome;
+    @NotBlank(message = "O campo nome de usuário não pode ser vazio.")
+    private String nomeUsuario;
+    @NotBlank(message = "O campo e-mail não pode ser vazio.")
+    private String email;
+    @NotBlank(message = "O campo senha não pode ser vazio.")
+    private String senha;
 
-    }
+}
