3e7e84666f66cf00bda805229d3e68a9e4277d96
alexandrejnrx <alexandre.jnrx@gmail.com>
2025-10-15 12:53:47 -0300

    feat(seguranca): adiciona dependencia do Spring Security e configura classe ConfiguracaoSeguranca para liberar todas as rotas

---
+++ src/main/java/br/com/alexandrejnrx/controlprodapi/controller/ConfiguracaoSeguranca.java
@@ -0,0 +1,20 @@
+package br.com.alexandrejnrx.controlprodapi.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ConfiguracaoSeguranca {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll())
+                .build();
+    }
+}
