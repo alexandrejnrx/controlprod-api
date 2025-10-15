907a32c603d164165aa238a52c46e8316c041496
alexandrejnrx <alexandre.jnrx@gmail.com>
2025-10-15 20:45:29 -0300

    refactor(migration): altera nome campo 'serial' para 'numero_serie'

--- src/main/resources/db/migration/V2__cria-tabela-produtos.sql
+++ src/main/resources/db/migration/V2__cria-tabela-produtos.sql
@@ -2,7 +2,7 @@ CREATE TABLE produtos
 (
     id                    INT AUTO_INCREMENT PRIMARY KEY,
     numero_unico_producao INT          NOT NULL UNIQUE,
-    serial                INT UNIQUE,
+    numero_serie                INT UNIQUE,
     data_producao         DATE,
     lote                  INT          NOT NULL,
     responsavel           VARCHAR(100) NOT NULL
