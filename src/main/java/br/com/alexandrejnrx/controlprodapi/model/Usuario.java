13564a7ce0bf94a69f3643a70d1423bcdb8ed44c
alexandrejnrx <alexandre.jnrx@gmail.com>
2025-10-15 16:08:36 -0300

    feat(usuario): atualiza entidade Usuario

--- src/main/java/br/com/alexandrejnrx/controlprodapi/model/Usuario.java
+++ src/main/java/br/com/alexandrejnrx/controlprodapi/model/Usuario.java
@@ -13,9 +13,13 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(nullable = false, length = 100)
     private String nome;
+    @Column(name = "nome_usuario", nullable = false, length = 20, unique = true)
     private String nomeUsuario;
+    @Column(nullable = false, length = 100, unique = true)
     private String email;
+    @Column(nullable = false, length = 255)
     private String senha;
 
 }
---
+++ src/main/resources/db/migration/V1__cria-tabela-usuarios.sql
@@ -0,0 +1,8 @@
+CREATE TABLE usuarios
+(
+    id           INT AUTO_INCREMENT PRIMARY KEY,
+    nome         VARCHAR(100),
+    nome_usuario VARCHAR(20)  NOT NULL UNIQUE,
+    email        VARCHAR(100) NOT NULL UNIQUE,
+    senha        VARCHAR(255) NOT NULL
+);
