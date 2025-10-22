fa0f4025e67d81db4469974d2be4b719ed7d4334
alexandrejnrx <alexandre.jnrx@gmail.com>
2025-10-22 12:46:32 -0300

    feat(security): adiciona autenticacao para rotas

--- src/main/java/br/com/alexandrejnrx/controlprodapi/configuration/ConfiguracaoSeguranca.java
+++ src/main/java/br/com/alexandrejnrx/controlprodapi/configuration/ConfiguracaoSeguranca.java
@@ -1,26 +1,48 @@
 package br.com.alexandrejnrx.controlprodapi.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class ConfiguracaoSeguranca {
+
+    @Autowired
+    SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll())
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/listar-usuarios").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/cadastrar-usuario").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/deletar-usuario/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/alterar-dados/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
