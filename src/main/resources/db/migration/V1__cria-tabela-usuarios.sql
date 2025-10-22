a2ad06719519a4f8c35b32b6974491cd8aedc9de
alexandrejnrx <alexandre.jnrx@gmail.com>
2025-10-22 12:52:01 -0300

    feat(usuario): adiciona coluna perfil

--- src/main/resources/db/migration/V1__cria-tabela-usuarios.sql
+++ src/main/resources/db/migration/V1__cria-tabela-usuarios.sql
@@ -4,5 +4,6 @@ CREATE TABLE usuarios
     nome         VARCHAR(100),
     nome_usuario VARCHAR(20)  NOT NULL UNIQUE,
     email        VARCHAR(100) NOT NULL UNIQUE,
-    senha        VARCHAR(255) NOT NULL
+    senha        VARCHAR(255) NOT NULL,
+    perfil       VARCHAR(10)  NOT NULL
 );
