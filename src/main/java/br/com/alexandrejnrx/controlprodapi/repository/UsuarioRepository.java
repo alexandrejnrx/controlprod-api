d57623f1e31acc66c49778f3ee37cca1f495fc81
alexandrejnrx <alexandre.jnrx@gmail.com>
2025-10-22 12:53:06 -0300

    feat(usuario): adiciona findByNomeUsuario

--- src/main/java/br/com/alexandrejnrx/controlprodapi/repository/UsuarioRepository.java
+++ src/main/java/br/com/alexandrejnrx/controlprodapi/repository/UsuarioRepository.java
@@ -2,11 +2,14 @@ package br.com.alexandrejnrx.controlprodapi.repository;
 
 import br.com.alexandrejnrx.controlprodapi.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
     boolean existsByNomeUsuario(String nomeUsuario);
 
     boolean existsByEmail(String email);
+
+    UserDetails findByNomeUsuario(String nomeUsuario);
 
 }
