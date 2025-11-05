1945ba576b9cf1cab4af2e387c79106cf0a132e4
alexandrejnrx <alexandre.jnrx@gmail.com>
2025-11-05 15:43:08 -0300

    feat(project): implementa nova entidade

--- src/main/java/br/com/alexandrejnrx/controlprodapi/model/Project.java
+++ src/main/java/br/com/alexandrejnrx/controlprodapi/model/Project.java
@@ -1,4 +1,24 @@
 package br.com.alexandrejnrx.controlprodapi.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "projects")
+@Getter
+@Setter
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false, length = 50)
+    private String name;
+    @ManyToOne
+    @JoinColumn(name = "client_id", nullable = false)
+    private Client client;
+    @Column(nullable = false)
+    private Boolean active;
+
 }
