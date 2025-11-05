ecd548a18deefb42e73a57424da7e9b49f6dda55
alexandrejnrx <alexandre.jnrx@gmail.com>
2025-11-05 15:48:01 -0300

    feat(technicalAssistance): implementa nova entidade

--- src/main/java/br/com/alexandrejnrx/controlprodapi/model/TechnicalAssistance.java
+++ src/main/java/br/com/alexandrejnrx/controlprodapi/model/TechnicalAssistance.java
@@ -1,4 +1,35 @@
 package br.com.alexandrejnrx.controlprodapi.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "technical_assistance")
+@Getter
+@Setter
 public class TechnicalAssistance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false)
+    private LocalDate date;
+    @Column(nullable = false, length = 50)
+    private String requester;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 10)
+    private DefectOrigin origin;
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+    @Column(name = "defect_reason", nullable = false, length = 150)
+    private String defectReason;
+    @Column(nullable = false, length = 50)
+    private String responsible;
+    @Column(length = 1000)
+    private String resolution;
+
 }
